2e806562c8a9d06b126b50b9d97157c6d9379686
maxstar15 <sealvarster@gmail.com>
2021-11-21 00:59:59 -0600

    Reorganizado el orden de la pagina

--- codigo.js
+++ codigo.js
@@ -3,7 +3,7 @@ function Login() {
   var usuario = document.login.usuario.value;
   var password = document.login.password.value;
   if (usuario == "admin" && password == "1234") {
-    window.location = "portafolio_miguel/mainPage.html";
+    window.location = "index.html";
   }
   if (usuario == "admin2" && password == "pass2") {
     window.location = "registro.html";
